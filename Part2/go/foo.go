4480b4d210821f7c7e904628dcf6860b047d677f
kjersbry <kjersbry@stud.ntnu.no>
2019-01-23 12:28:54 +0100

    lol

--- Part2/go/foo.go
+++ Part2/go/foo.go
@@ -12,6 +12,8 @@ const (
 	GetNumber = iota
 	Exit
 )
+//ch := make(chan int)
+
 
 func number_server(add_number <-chan int, control <-chan int, number chan<- int) {
 	var i = 0
@@ -21,8 +23,19 @@ func number_server(add_number <-chan int, control <-chan int, number chan<- int)
 		select {
 			// TODO: receive different messages and handle them correctly
 			// You will at least need to update the number and handle control signals.
-		}
-	}
+      //go incrementing()
+      //go decrementing()
+      case update_number:= <- add_number:
+        i += update_number
+      case complete := <- control:
+        if complete == GetNumber{
+          number <- i
+        }
+        if complete == Exit{
+          return
+      }
+    }
+  }
 }
 
 func incrementing(add_number chan<-int, finished chan<- bool) {
@@ -30,6 +43,8 @@ func incrementing(add_number chan<-int, finished chan<- bool) {
 		add_number <- 1
 	}
 	//TODO: signal that the goroutine is finished
+  finished <- true
+
 }
 
 func decrementing(add_number chan<- int, finished chan<- bool) {
@@ -37,6 +52,7 @@ func decrementing(add_number chan<- int, finished chan<- bool) {
 		add_number <- -1
 	}
 	//TODO: signal that the goroutine is finished
+  finished <- true
 }
 
 func main() {
@@ -44,10 +60,21 @@ func main() {
 
 	// TODO: Construct the required channels
 	// Think about wether the receptions of the number should be unbuffered, or buffered with a fixed queue size.
+      add_number := make(chan int)
+      control := make(chan int)
+      number := make(chan int)
+      finished := make(chan bool)
+
 
 	// TODO: Spawn the required goroutines
-
+      go number_server(add_number, control, number)
+      go incrementing(add_number,finished)
+      go decrementing(add_number,finished)
 	// TODO: block on finished from both "worker" goroutines
+
+  <-finished
+  <-finished
+
 
 	control<-GetNumber
 	Println("The magic number is:", <- number)
--- Part2/python/foo.py
+++ Part2/python/foo.py
@@ -2,19 +2,29 @@
 # python fo.py
 
 from threading import Thread
+import threading
 
 # Potentially useful thing:
 #   In Python you "import" a global variable, instead of "export"ing it when you declare it
 #   (This is probably an effort to make you feel bad about typing the word "global")
+lock = threading.Lock()
 i = 0
 
 def incrementingFunction():
     global i
     # TODO: increment i 1_000_000 times
+    for j in range(1000000):
+        lock.acquire()
+        i += 1
+        lock.release()
 
 def decrementingFunction():
     global i
     # TODO: decrement i 1_000_000 times
+    for j in range(1000000):
+        lock.acquire()
+        i-=1
+        lock.release()
 
 
 
@@ -23,12 +33,14 @@ def main():
 
     incrementing = Thread(target = incrementingFunction, args = (),)
     decrementing = Thread(target = decrementingFunction, args = (),)
-    
+
     # TODO: Start both threads
-    
+    incrementing.start()
+    decrementing.start()
+
     incrementing.join()
     decrementing.join()
-    
+
     print("The magic number is %d" % (i))
 
 
